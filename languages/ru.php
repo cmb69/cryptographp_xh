c462d35cb945af523c6c0fdb61fbb1d064ee6eac
Christoph M. Becker <cmbecker69@gmx.de>
2015-02-13 14:34:40 +0100

    added Russion localization (contributed by Old)

---
+++ languages/ru.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * Copyright (c) 2012 Old aka Lybomyr Kydray
+ *
+ * Translation are licensed under the GNU General Public License, version 2 or
+ * later.
+ */
+
+$plugin_tx['cryptographp']['alt_image']="Визуальный запрос";
+$plugin_tx['cryptographp']['alt_reload']="Получить новый запрос";
+$plugin_tx['cryptographp']['alt_audio']="Получить аудио запрос";
+$plugin_tx['cryptographp']['message_enter_code']="Введите код защиты от спама:";
+$plugin_tx['cryptographp']['syscheck_title']="Проверка системы";
+$plugin_tx['cryptographp']['syscheck_phpversion']="Версия PHP &ge; %s";
+$plugin_tx['cryptographp']['syscheck_extension']="Расширение '%s' загружено";
+$plugin_tx['cryptographp']['syscheck_magic_quotes']="Кодировка 'UTF-8' установлена";
+$plugin_tx['cryptographp']['syscheck_freetype_support']="Шрифт TrueType доступен";
+$plugin_tx['cryptographp']['syscheck_gif_support']="Формат GIF поддерживается";
+$plugin_tx['cryptographp']['syscheck_jpeg_support']="Формат JPEG поддерживается";
+$plugin_tx['cryptographp']['syscheck_png_support']="Формат PNG поддерживается";
+$plugin_tx['cryptographp']['syscheck_encoding']="Кодировка 'UTF-8' установлена";
+$plugin_tx['cryptographp']['syscheck_writable']="Папка '%s' доступна для записи";
+$plugin_tx['cryptographp']['error_cookies']="Необходимо разрешить куки (сookies!";
+$plugin_tx['cryptographp']['error_user_time']="Очень быстрая перезагрузка!";
+$plugin_tx['cryptographp']['error_audio']="Аудио капча не генерирована! Пожалуйста, получите новый запрос и повторите попытку.";
+$plugin_tx['cryptographp']['cf_crypt_width']="Ширина криптограммы в пикселях";
+$plugin_tx['cryptographp']['cf_crypt_height']="Высота  криптограммы в пикселях";
+$plugin_tx['cryptographp']['cf_bg_rgb_red']="Цвет фона (RGB): насыщенность красного цвета";
+$plugin_tx['cryptographp']['cf_bg_rgb_green']="Цвет фона (RGB): насыщенность зеленого цвета";
+$plugin_tx['cryptographp']['cf_bg_rgb_blue']="Цвет фона (RGB): насыщенность синего цвета";
+$plugin_tx['cryptographp']['cf_bg_clear']="Сделать фон прозрачным (только формат PNG): <em>да</em> или <em>нет</em>";
+$plugin_tx['cryptographp']['cf_bg_image']="Имя файла изображения фона. При необходимости, изображение для криптограммы будет изменено. Если имя не указано, из папки будет выбрано случайное изображение. Укажите расположение относительно папки плагина.";
+$plugin_tx['cryptographp']['cf_bg_frame']="Добавить рамку вокруг пиктограммы: <em>да</em> или <em>нет</em>";
+$plugin_tx['cryptographp']['cf_char_rgb_red']="Цвет символов (RGB): насыщенность красного цвета";
+$plugin_tx['cryptographp']['cf_char_rgb_green']="Цвет символов (RGB): насыщенность зеленого цвета";
+$plugin_tx['cryptographp']['cf_char_rgb_blue']="Цвет символов (RGB):  насыщенность синего цвета";
+$plugin_tx['cryptographp']['cf_char_color_random']="Выбирать случайный цвет: <em>да</em> или <em>нет</em>";
+$plugin_tx['cryptographp']['cf_char_color_random_level']="Яркость случайного цвета: <em>0</em> (без ограничений), <em>1</em> (только очень светлые цвета), <em>2</em> (только светлые цвета), <em>3</em> (только темные цвета) or <em>4</em> (только очень темные цвета)";
+$plugin_tx['cryptographp']['cf_char_clear']="Прозрачность символов: от <em>0</em> (непрозрачный) до <em>127</em> (полностью прозрачный). Необходим PHP >= 4.3.2";
+$plugin_tx['cryptographp']['cf_fonts']="Разделенный точкой с запятой список шрифтов TrueType, доступных в папке шрифтов (<b>fonts/<b/>)";
+$plugin_tx['cryptographp']['cf_char_allowed']="Разрешить символы, если для режима \"crypt easy/легкое шифрование\" указано \"нет\" (аудио капча поддерживает только A-Z и 0-9).";
+$plugin_tx['cryptographp']['cf_crypt_easy']="Криптограмма может содержать гласные и согласные: <em>да</em> или <em>нет</em>";
+$plugin_tx['cryptographp']['cf_char_allowed_consonants']="Разрешить согласные, если для режима \"crypt easy/легкое шифрование\" указано \"нет\" (аудио капча поддерживает только A-Z и 0-9).";
+$plugin_tx['cryptographp']['cf_char_allowed_vowels']="Разрешить гласные, если для режима \"crypt easy/легкое шифрование\" указано \"нет\" (аудио капча поддерживает только A-Z и 0-9).";
+$plugin_tx['cryptographp']['cf_char_count_min']="Минимальное количество символов в криптограмме";
+$plugin_tx['cryptographp']['cf_char_count_max']="Максимальное количество символов в криптограмме";
+$plugin_tx['cryptographp']['cf_char_space']="Расстояние между символами в пикселях";
+$plugin_tx['cryptographp']['cf_char_size_min']="Минимальный размер шрифта для символов";
+$plugin_tx['cryptographp']['cf_char_size_max']="Максимальный размер шрифта для символов";
+$plugin_tx['cryptographp']['cf_char_angle_max']="Максимальный угол наклона шрифта";
+$plugin_tx['cryptographp']['cf_char_displace']="Случайное смещение символов в вертикальном направлении: <em>да</em> или <em>нет</em>";
+$plugin_tx['cryptographp']['cf_crypt_gaussian_blur']="Размытие символов с использованием метода Гаусса: <em>да</em> или <em>нет</em>. Необходим PHP > 5.0.0";
+$plugin_tx['cryptographp']['cf_crypt_gray_scale']="Конвертирование изображения в оттенки серого: <em>да</em> или <em>нет</em>. Необходим PHP > 5.0.0";
+$plugin_tx['cryptographp']['cf_noise_pixel_min']="Минимальное количество случайных пикселей";
+$plugin_tx['cryptographp']['cf_noise_pixel_max']="Максимальное количество случайных пикселей";
+$plugin_tx['cryptographp']['cf_noise_line_min']="Минимальное количество случайных линий";
+$plugin_tx['cryptographp']['cf_noise_line_max']="Максимальное количество случайных линий";
+$plugin_tx['cryptographp']['cf_noise_circle_min']="Минимальное количество случайных окружностей";
+$plugin_tx['cryptographp']['cf_noise_circle_max']="Максимальное количество случайных окружностей";
+$plugin_tx['cryptographp']['cf_noise_color_char']="Цвет шума: <em>1</em> (цвет символа), <em>2</em> (цвет фона) или <em>3</em> (случайный цвет)";
+$plugin_tx['cryptographp']['cf_noise_brush_size']="Толщина линий и окружностей";
+$plugin_tx['cryptographp']['cf_noise_above']="\"Зашумленность\" символов: <em>да</em> или <em>нет</em>";
+$plugin_tx['cryptographp']['cf_crypt_format']="Формат файлов изображений: <em>gif</em>, <em>png</em> или <em>jpeg</em>";
+$plugin_tx['cryptographp']['cf_crypt_use_timer']="Доступная задержка (секунд) перед перезагрузкой новой криптограммы";
+$plugin_tx['cryptographp']['cf_crypt_use_timer_error']="Сообщать об ошибках, если криптограмма перезагружена слишком быстро (\"да\") или подождать (\"нет\")";
+$plugin_tx['cryptographp']['cf_crypt_expiration']="Продолжительность использования криптограммы (секунд)";
+
+?>
