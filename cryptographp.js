c4ef862c7e0ce3a5fd231ae2ad6c72f1ba2b6513
Christoph M. Becker <cmbecker69@gmx.de>
2017-03-08 00:18:20 +0100

    Switch to GNU style file headers

--- cryptographp.js
+++ cryptographp.js
@@ -1,11 +1,21 @@
 /*!
- * The JavaScript of Cryptographp_XH.
+ * Copyright 2006-2007 Sylvain Brison
+ * Copyright 2011-2017 Christoph M. Becker
  *
- * @author    Sylvain Brison <cryptographp@alphpa.com>
- * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2006-2007 Sylvain Brison
- * @copyright 2011-2017 Christoph M. Becker <http://3-magi.net>
- * @link      http://3-magi.net/?CMSimple_XH/Cryptographp_XH
+ * This file is part of Cryptographp_XH.
+ *
+ * Cryptographp_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cryptographp_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cryptographp_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 var CRYPTOGRAPHP = CRYPTOGRAPHP || {};
--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
 
 /**
- * @copyright 2006-2007 Sylvain Brison
- * @copyright 2011-2017 Christoph M. Becker <http://3-magi.net>
- * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * Copyright 2006-2007 Sylvain Brison
+ * Copyright 2011-2017 Christoph M. Becker
+ *
+ * This file is part of Cryptographp_XH.
+ *
+ * Cryptographp_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cryptographp_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cryptographp_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 if (!defined('CMSIMPLE_XH_VERSION')
